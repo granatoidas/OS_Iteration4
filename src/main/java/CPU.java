3e4de5873c418cbc091a970d2cdaf0cc156d0d12
granatoidas <granto.mail@gmail.com>
2017-05-08 13:13:06 +0300

    Pridejau dar š, bereikalingas commit
    Bet zinai - kad neprapultu kaip pvz su SV f****n N

--- src/main/java/CPU.java
+++ src/main/java/CPU.java
@@ -1,6 +1,43 @@
+import java.util.ArrayList;
 
 public class CPU {
+	ArrayList<Process> procesai = new ArrayList<Process>();
+	ArrayList<Resource> resursai = new ArrayList<Resource>();
+	
+	
+	void planutojas(){
+		
+	}
+	
+	void skirstytojas(){
+		
+	}
+	
+	//procesu primityvai
 	void kurtiProcesa(int parentId, String name){
+		
+	}
+	void naikintiProcesa(int id){
+		
+	}
+	void stabdytiProcesa(int id){
+		
+	}
+	void aktyvuotiProcesa(int id){
+		
+	}
+	
+	//resursu primityvai
+	void kurtiResursa(int parentId, String name){
+		
+	}
+	void naikintiResursa(int id){
+		
+	}
+	void prasytiResurso(int id){
+		
+	}
+	void atalaisvintiResursa(int id){
 		
 	}
 }
--- src/main/java/Resource.java
+++ src/main/java/Resource.java
@@ -1,4 +1,18 @@
 
 public class Resource {
-
+	int id;
+	int parentId;
+	String name;
+	
+	Integer adresatoId;
+	
+	public Resource(int parentId, String name){
+		this(parentId, name, (Integer) null);
+	}
+	
+	public Resource(int parentId, String name, int adresatoId){
+		this.parentId = parentId;
+		this.name = name;
+		this.adresatoId = adresatoId;
+	}
 }
